27a00eb2e3759003c49dc47e3d396c435828a419
LackingCuriosity <123216154+LackingCuriosity@users.noreply.github.com>
2025-07-01 23:31:29 -0400

    Deployment final final

--- studyApp/myApp/static/myApp/register.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    console.log("DSd")
-    if (document.getElementById("messageDiv").innerHTML == "") {
-        document.getElementById("messageDiv").innerHTML = "WARNING THIS SITE WAS NOT BUILT WITH SECURITY AND CRYPTOGRAPY IN MIND. PLEASE DO NOT USE THE SAME OR SIMILAR CREDENTIALS AS OTHER SITES"
-}
-})
